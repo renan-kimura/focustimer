896fc6992707d0d64f8685fb94f1bf46673f10b5
Renan Kimura <kimura@live.it>
2023-09-03 15:35:00 -0300

    fix empilhamento de timeout

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -4,6 +4,8 @@ import { reset } from "./actions.js";
 import { kichenTimer } from "./sounds.js";
 
 export function countdown(){
+    clearTimeout(state.countdownId)
+    
     if(!state.isRunning){
         return
     }
@@ -22,7 +24,7 @@ export function countdown(){
     }
     updateDisplay(minutes,seconds)
 
-    setTimeout(() => countdown(), 1000)
+    state.countdownId = setTimeout(() => countdown(), 1000)
 }
 export function updateDisplay(minutes, seconds){
     minutes = minutes ?? state.minutes
