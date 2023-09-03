eaafbeb0acec7883c1f4fd3556e99f293408ba09
Renan Kimura <kimura@live.it>
2023-09-03 12:58:35 -0300

    correção css dos botões

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,3 +1,15 @@
+import state from "./state.js";
+
 export function toggleRunning(){
-    console.log('toggleRunning fuction');
+    state.isRunning = document.documentElement.classList.toggle('running')
+}
+export function reset(){
+    state.isRunning = false
+    document.documentElement.classList.remove('running')
+}
+export function set(){
+
+}
+export function toggleMusic(){
+    state.isMute = document.documentElement.classList.toggle('music-on')
 }
--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -4,9 +4,9 @@ import * as actions from './actions.js';
 export function registerControls(){
     controls.addEventListener('click', (event) => {
         const action = event.target.dataset.action
-        if(typeof action[action] != "function"){
+        if(typeof actions[action] != "function"){
             return
         }
-        action[action]
+        actions[action]()
     })
 }
